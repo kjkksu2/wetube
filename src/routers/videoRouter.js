9b2ae371fdf4f4b81e7d54b5aec99f2992c023a8
kjkksu2 <80094949+kjkksu2@users.noreply.github.com>
2021-07-08 16:45:39 +0900

    update router

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,8 +1,19 @@
 import express from "express";
-import { editVideo } from "../controllers/videoController";
+import {
+  editVideo,
+  getUploadVideo,
+  postUploadVideo,
+} from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id", editVideo);
+videoRouter.get("/:id([0-9a-f]{24})", editVideo);
+videoRouter.get("/:id([0-9a-f]{24})/upload-video", getUploadVideo);
+videoRouter.post(
+  "/:id([0-9a-f]{24})/upload-video",
+  uploadVideo.single("videoFile"),
+  postUploadVideo
+);
 
 export default videoRouter;
