b8b32aabf5fc6b16367dd5cad3d44ced4ade9100
kjkksu2 <80094949+kjkksu2@users.noreply.github.com>
2021-07-08 14:40:22 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import rootRouter from "./routers/rootRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
 const handleListen = () => console.log("✅ Listening on port 4000");
+
+app.use("/", rootRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 app.listen(4000, handleListen);
