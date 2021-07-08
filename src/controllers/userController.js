e8445fda3cf7092aea688b9f619633d5b491bb1c
kjkksu2 <80094949+kjkksu2@users.noreply.github.com>
2021-07-08 16:45:04 +0900

    update controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,10 @@
-export const join = (req, res) => res.send("join");
-export const login = (req, res) => res.send("login");
+export const join = (req, res) => {
+  return res.render("users/join", { pageTitle: "join" });
+};
+export const login = (req, res) => {
+  return res.render("users/login", { pageTitle: "login" });
+};
 export const logout = (req, res) => res.send("logout");
-export const editProfile = (req, res) => res.send("edit-profile");
+export const editProfile = (req, res) => {
+  return res.render("users/editProfile", { pageTitle: "edit-profile" });
+};
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,28 @@
+import mongoose from "mongoose";
+import Video from "../models/Video";
+
 export const home = (req, res) => {
-  return res.render("home");
+  return res.render("videos/home", { pageTitle: "Home" });
 };
-export const search = (req, res) => res.send("search");
+export const search = (req, res) => {
+  return res.render("videos/search", { pageTitle: "Search" });
+};
 export const editVideo = (req, res) => res.send("edit-video");
+
+export const getUploadVideo = (req, res) => {
+  return res.render("videos/uploadVideo", { pageTitle: "Upload Video" });
+};
+export const postUploadVideo = async (req, res) => {
+  const {
+    file: { path },
+    body: { title, description },
+  } = req;
+
+  const video = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+  });
+  console.log(video);
+  return res.render("videos/uploadVideo", { pageTitle: "Upload Video" });
+};
