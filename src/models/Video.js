1959e5858473ba08f77faf398dc61f2f50dbb0da
kjkksu2 <80094949+kjkksu2@users.noreply.github.com>
2021-07-08 23:37:52 +0900

    update video model

--- src/models/Video.js
+++ src/models/Video.js
@@ -10,6 +10,10 @@ const videoModel = new mongoose.Schema({
     required: true,
   },
   description: String,
+  views: {
+    type: Number,
+    default: 0,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
