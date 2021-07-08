90c00e90f2c77af0f0434fbc88b672d4c45928f7
kjkksu2 <80094949+kjkksu2@users.noreply.github.com>
2021-07-08 16:45:44 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -7,6 +8,9 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
