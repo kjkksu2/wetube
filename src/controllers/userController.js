02ff1880724b7c2a3e76c0c59b2670341dcb4294
kjkksu2 <80094949+kjkksu2@users.noreply.github.com>
2021-07-08 15:07:56 +0900

    update controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,4 @@
+export const join = (req, res) => res.send("join");
+export const login = (req, res) => res.send("login");
+export const logout = (req, res) => res.send("logout");
 export const editProfile = (req, res) => res.send("edit-profile");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,1 +1,5 @@
+export const home = (req, res) => {
+  return res.render("home");
+};
+export const search = (req, res) => res.send("search");
 export const editVideo = (req, res) => res.send("edit-video");
