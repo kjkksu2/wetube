0e378451673f30cd8e891b379eac82035bc728a1
kjkksu2 <80094949+kjkksu2@users.noreply.github.com>
2021-07-08 15:08:16 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
