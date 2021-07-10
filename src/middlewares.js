c9fa36cddb73f9b379be595dc821f16bc3686725
kjkksu2 <80094949+kjkksu2@users.noreply.github.com>
2021-07-11 02:25:40 +0900

    res.locals 추가

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,3 +1,9 @@
 import multer from "multer";
 
+export const middlewareLocals = (req, res, next) => {
+  res.locals.loggedIn = req.session.loggedIn;
+  res.locals.user = req.session.user;
+  next();
+};
+
 export const uploadVideo = multer({ dest: "uploads/videos" });
