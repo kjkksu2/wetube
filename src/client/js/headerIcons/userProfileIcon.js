ca09d7a4420dfd0637b81189d9318d0d2e6178f3
kjkksu2 <80094949+kjkksu2@users.noreply.github.com>
2021-07-19 00:08:05 +0900

    userProfile

---
+++ src/client/js/headerIcons/userProfileIcon.js
@@ -0,0 +1,20 @@
+const bodyClick = document.querySelector("body");
+const userProfileIcon = document.querySelector(".user__nav--profile");
+const userProfileContent = document.querySelector(".userProfile");
+
+const HIDDEN_CLASS = "hidden";
+
+const handleProfile = (event) => {
+  if (!userProfileContent.classList.contains(HIDDEN_CLASS)) {
+    userProfileContent.classList.add(HIDDEN_CLASS);
+  } else if (
+    userProfileContent.classList.contains(HIDDEN_CLASS) &&
+    event.target.localName == "img"
+  ) {
+    userProfileContent.classList.remove(HIDDEN_CLASS);
+  }
+};
+
+if (userProfileIcon) {
+  bodyClick.addEventListener("click", handleProfile);
+}
