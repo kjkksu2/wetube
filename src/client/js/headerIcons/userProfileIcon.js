7cf6c7232894b1f9c13178f8f1f1c0e9b863b007
kjkksu2 <80094949+kjkksu2@users.noreply.github.com>
2021-07-20 19:32:36 +0900

    stop scroll

--- src/client/js/headerIcons/userProfileIcon.js
+++ src/client/js/headerIcons/userProfileIcon.js
@@ -1,20 +1,23 @@
-const bodyClick = document.querySelector("body");
+const body = document.querySelector("body");
 const userProfileIcon = document.querySelector(".user__nav--profile");
 const userProfileContent = document.querySelector(".userProfileIcon");
 
 const HIDDEN_CLASS = "hidden";
+const SCROLL_CLASS = "hideScroll";
 
 const handleProfile = (event) => {
   if (!userProfileContent.classList.contains(HIDDEN_CLASS)) {
     userProfileContent.classList.add(HIDDEN_CLASS);
+    body.classList.remove(SCROLL_CLASS);
   } else if (
     userProfileContent.classList.contains(HIDDEN_CLASS) &&
     event.target.localName == "img"
   ) {
     userProfileContent.classList.remove(HIDDEN_CLASS);
+    body.classList.add(SCROLL_CLASS);
   }
 };
 
 if (userProfileIcon) {
-  bodyClick.addEventListener("click", handleProfile);
+  body.addEventListener("click", handleProfile);
 }
--- src/client/js/headerIcons/videoIcon.js
+++ src/client/js/headerIcons/videoIcon.js
@@ -1,20 +1,23 @@
-const bodyClick = document.querySelector("body");
+const body = document.querySelector("body");
 const videoIcon = document.querySelector(".fa-video");
 const videoChoice = document.querySelector(".video__choice");
 
 const HIDDEN_CLASS = "hidden";
+const SCROLL_CLASS = "hideScroll";
 
 const handleVideoIcon = (event) => {
   if (!videoChoice.classList.contains(HIDDEN_CLASS)) {
     videoChoice.classList.add(HIDDEN_CLASS);
+    body.classList.remove(SCROLL_CLASS);
   } else if (
     videoChoice.classList.contains(HIDDEN_CLASS) &&
     event.target.className == "fas fa-video"
   ) {
     videoChoice.classList.remove(HIDDEN_CLASS);
+    body.classList.add(SCROLL_CLASS);
   }
 };
 
 if (videoIcon) {
-  bodyClick.addEventListener("click", handleVideoIcon);
+  body.addEventListener("click", handleVideoIcon);
 }
